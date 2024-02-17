89ebce34617b5782409bbe71f321d280f683136b
Alex-La <55190911+Alex-La@users.noreply.github.com>
2024-02-17 15:37:41 +0700

    file browser

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,5 @@
+return {
+    { "MunifTanjim/nui.nvim", lazy = true }, -- UI components
+    { "nvim-lua/plenary.nvim", lazy = true }, -- Lua utilities
+    { "nvim-tree/nvim-web-devicons", lazy = true } -- Devicons
+}
